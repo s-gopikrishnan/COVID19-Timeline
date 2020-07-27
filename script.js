7e5189b1c419259c1e43b4933640e3d697acc830
Gopikrishnan Srinivasan <gopi.s.krishnan@gmail.com>
2020-07-27 13:10:30 -0500

    Timeline - us and states

--- script.js
+++ script.js
@@ -1,86 +1,68 @@
-var width = 960;
-var height = 500;
-
-// D3 Projection
-var projection = d3.geo.albersUsa()
-    .translate([width / 2, height / 2])    // translate to center of screen
-    .scale([1000]);          // scale things down so see entire US
-// Define path generator
-var path = d3.geo.path()               // path generator that will convert GeoJSON to SVG paths
-    .projection(projection);  // tell path generator to use albersUsa projection
-
-
-// Define linear scale for output
-var color = d3.scale.linear()
-    .range(["rgb(213,222,217)", "rgb(69,173,168)", "rgb(84,36,55)", "rgb(217,91,67)"]);
-
-var legendText = ["Cities Lived", "States Lived", "States Visited", "Nada"];
-
-//Create SVG element and append map to the SVG
-var svg = d3.select("body")
-    .append("svg")
-    .attr("width", width)
-    .attr("height", height);
-
-// Append Div for tooltip to SVG
-var div = d3.select("body")
-    .append("div")
-    .attr("class", "tooltip")
-    .style("opacity", 0);
-
-// Load in my states data!
 d3.csv("assets/stateslived.csv", function (data) {
 
-    color.domain([0, 1, 2, 3]); // setting the range of the input data
+    var width = 960;
+    var height = 500;
 
-    // Load GeoJSON data and merge with states data
+    var projection = d3.geoAlbersUsa().scale(1000).translate([width / 2, height / 2]);
+
+    var path = d3.geoPath().projection(projection);
+
+    //Create SVG element and append map to the SVG
+    var svg = d3.select("#map-div")
+        .append("svg")
+        .attr("width", width)
+        .attr("height", height);
+
+    // Append Div for tooltip to SVG
+    var div = d3.select("#map-div")
+        .append("div")
+        .attr("class", "tooltip")
+        .style("opacity", 0);
+
+    // Define linear scale for output
+    var color = d3.scaleLinear()
+        .range(["rgb(213,222,217)", "rgb(69,173,168)", "rgb(84,36,55)", "rgb(217,91,67)"]);
+
+    var legendText = ["Cities Lived", "States Lived", "States Visited", "Nada"];
+
     d3.json("assets/us-states.json", function (json) {
-        console.log(json);
-        // Loop through each state data value in the .csv file
         for (var i = 0; i < data.length; i++) {
-
-            // Grab State Name
             var dataState = data[i].state;
-
-            // Grab data value 
             var dataValue = data[i].visited;
-
-            // Find the corresponding state inside the GeoJSON
             for (var j = 0; j < json.features.length; j++) {
                 var jsonState = json.features[j].properties.name;
-
                 if (dataState == jsonState) {
-
-                    // Copy the data value into the JSON
                     json.features[j].properties.visited = dataValue;
-
-                    // Stop looking through the JSON
                     break;
                 }
             }
         }
 
-        // Bind the data to the SVG and create one path per GeoJSON feature
+        console.log("json.features.length: " + json.features.length);
+
+        color.domain([0, 1, 2, 3]);
         svg.selectAll("path")
             .data(json.features)
             .enter()
             .append("path")
             .attr("d", path)
-            .style("stroke", "#fff")
-            .style("stroke-width", "1")
-            .style("fill", function (d) {
-
+            .style("stroke", "rgb(217,91,67)")
+            .style("stroke-width", "1");
+            
+            /* .style("fill", function (d) {
+                console.log("hello");
                 // Get data value
                 var value = d.properties.visited;
 
-                if (value) {
+                if (value == 1) {
                     //If value exists…
+                    console.log(color(value));
                     return color(value);
                 } else {
                     //If value is undefined…
                     return "rgb(213,222,217)";
                 }
-            });
+            }); */
 
 
         // Map the cities I have lived in!
@@ -100,7 +82,8 @@ d3.csv("assets/stateslived.csv", function (data) {
                     return Math.sqrt(d.years) * 4;
                 })
                 .style("fill", "rgb(217,91,67)")
-                .style("opacity", 0.85)
+                .style("opacity", .5)
+                .style("stroke", "black")
 
                 // Modification of custom tooltip code provided by Malcolm Maclean, "D3 Tips and Tricks" 
                 // http://www.d3noob.org/2013/01/adding-tooltips-to-d3js-graph.html
@@ -122,10 +105,11 @@ d3.csv("assets/stateslived.csv", function (data) {
         });
 
         // Modified Legend Code from Mike Bostock: http://bl.ocks.org/mbostock/3888852
-        var legend = d3.select("body").append("svg")
+        var legend = d3.select("#map-div").append("svg")
             .attr("class", "legend")
             .attr("width", 140)
             .attr("height", 200)
+            .attr("custom-id", "legends-svg")
             .selectAll("g")
             .data(color.domain().slice().reverse())
             .enter()
@@ -143,6 +127,7 @@ d3.csv("assets/stateslived.csv", function (data) {
             .attr("y", 9)
             .attr("dy", ".35em")
             .text(function (d) { return d; });
+
     });
 
 });
